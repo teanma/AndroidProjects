45c5990a1adc3a0bd2cde45da58050c815e8c895
teanma <mardaleteodorandrei@gmail.com>
2021-10-29 13:46:34 +0200

    Upload
    Update

--- ProjectActivities/app/src/main/java/com/example/projectactivities/MainActivity.java
+++ ProjectActivities/app/src/main/java/com/example/projectactivities/MainActivity.java
@@ -5,8 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected static final String MAINACTIVITY_NAME = "com.example.projectactivities.name";
+    protected static final String MAINACTIVITY_SURNAME = "com.example.projectactivities.surname";
+    protected static final String MAINACTIVITY_AGE = "com.example.projectactivities.age";
+    protected static final String MAINACTIVITY_LICENSE = "com.example.projectactivities.license";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +22,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void cambiarActividad(View v) {
+    public void cargarDatos(View v) {
+
         Intent pantallaSecundaria = new Intent(this, MainActivity2.class);
-        startActivity(pantallaSecundaria);
+        EditText et_name = (EditText) findViewById(R.id.et_nombre);
+        EditText et_surname = (EditText) findViewById(R.id.et_apellidos);
+        EditText et_age = (EditText) findViewById(R.id.et_edad);
+        CheckBox cb_license = (CheckBox) findViewById(R.id.cb_carnet);
+
+        String message = null;
+        boolean start = false;
+        if (et_name.getText().toString().isEmpty()) {
+            message = "Introduce tu nombre";
+        } else {
+            String name = et_name.getText().toString();
+            pantallaSecundaria.putExtra(MAINACTIVITY_NAME, name);
+        }
+        if (et_surname.getText().toString().isEmpty()) {
+            message += " Introduce tu apellido";
+        } else {
+            String surname = et_surname.getText().toString();
+            pantallaSecundaria.putExtra(MAINACTIVITY_SURNAME, surname);
+        }
+        if (et_age.getText().toString().isEmpty()) {
+            message += " Introduce tu edad";
+        } else {
+            String age = et_age.getText().toString();
+            pantallaSecundaria.putExtra(MAINACTIVITY_AGE, age);
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        boolean license = cb_license.isChecked();
+        pantallaSecundaria.putExtra(MAINACTIVITY_LICENSE, license);
+
+        if (!et_name.getText().toString().isEmpty() || !et_surname.getText().toString().isEmpty() || !et_age.getText().toString().isEmpty()) {
+            startActivity(pantallaSecundaria);
+        } else {
+            message = null;
+        }
+
     }
 }
--- ProjectActivities/app/src/main/java/com/example/projectactivities/MainActivity2.java
+++ ProjectActivities/app/src/main/java/com/example/projectactivities/MainActivity2.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -12,6 +13,21 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent pantallaPrincipal = getIntent();
+        String name = getIntent().getStringExtra(MainActivity.MAINACTIVITY_NAME);
+        String surname = getIntent().getStringExtra(MainActivity.MAINACTIVITY_SURNAME);
+        String age = getIntent().getStringExtra(MainActivity.MAINACTIVITY_AGE);
+        String license = getIntent().getStringExtra(MainActivity.MAINACTIVITY_LICENSE);
+
+        TextView tv_name_result = (TextView) findViewById(R.id.tv_nom_resultado);
+        tv_name_result.setText(name);
+        TextView tv_sur_result = (TextView) findViewById(R.id.tv_ap_resultado);
+        tv_sur_result.setText(surname);
+        TextView tv_age_result = (TextView) findViewById(R.id.tv_edad_resultado);
+        tv_age_result.setText(age);
+        TextView tv_license_result = (TextView) findViewById(R.id.tv_carnet_resultado);
+        tv_license_result.setText(license);
     }
 
     public void cambiarActividad(View v) {
