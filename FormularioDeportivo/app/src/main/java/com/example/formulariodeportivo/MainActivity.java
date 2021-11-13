8ae2f5931b25d125531f41691504a824f12741d0
teanma <mardaleteodorandrei@gmail.com>
2021-11-14 00:21:16 +0100

    Update
    Upload

--- FormularioDeportivo/app/src/main/java/com/example/formulariodeportivo/MainActivity.java
+++ FormularioDeportivo/app/src/main/java/com/example/formulariodeportivo/MainActivity.java
@@ -14,22 +14,18 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements Listener {
 
     protected static final String MAINACTIVITY_NAME = "com.example.formulariodeportivo.name";
     protected static final String MAINACTIVITY_SURNAME = "com.example.formulariodeportivo.surname";
-    protected static final String MAINACTIVITY_PHONE = "com.example.formulariodeportivo.phone";
-    protected static final String MAINACTIVITY_EMAIL = "com.example.formulariodeportivo.email";
-    protected static final String MAINACTIVITY_FOOTBALL = "com.example.formulariodeportivo.football";
-    protected static final String MAINACTIVITY_BASKETBALL = "com.example.formulariodeportivo.basketball";
-    protected static final String MAINACTIVITY_PORTERO = "com.example.formulariodeportivo.portero";
-    protected static final String MAINACTIVITY_DEFENSA = "com.example.formulariodeportivo.defensa";
-    protected static final String MAINACTIVITY_MEDIO = "com.example.formulariodeportivo.medio";
-    protected static final String MAINACTIVITY_DELANTERO = "com.example.formulariodeportivo.delantero";
-    protected static final String MAINACTIVITY_BASE = "com.example.formulariodeportivo.base";
-    protected static final String MAINACTIVITY_ESCOLTA = "com.example.formulariodeportivo.escolta";
-    protected static final String MAINACTIVITY_ALERO = "com.example.formulariodeportivo.alero";
-    protected static final String MAINACTIVITY_PIVOT = "com.example.formulariodeportivo.pivot";
+    protected static final String MAINACTIVITY_SELECTED_CONTACT = "com.example.formulariodeportivo.selected_contact";
+    protected static final String MAINACTIVITY_CONTACT = "com.example.formulariodeportivo.contact";
+    protected static final String MAINACTIVITY_SPORT = "com.example.formulariodeportivo.sport";
+    protected static final String MAINACTIVITY_POSITION = "com.example.formulariodeportivo.position";
+
 
     TextView tv_name, tv_surname, tv_phone, tv_email, tv_football, tv_basketball, tv_position, tv_obligatory_field_name, tv_obligatory_field_surname, tv_obligatory_field_spinContact, tv_obligatory_field_phone, tv_obligatory_field_email, tv_obligatory_field_spinSport;
     EditText et_name, et_surname, et_phone, et_email;
@@ -217,55 +213,66 @@ public class MainActivity extends AppCompatActivity implements Listener {
         String surname = et_surname.getText().toString();
         String phone = et_phone.getText().toString();
         String email = et_email.getText().toString();
-        String football = tv_football.getText().toString();
-        String basketball = tv_basketball.getText().toString();
-        String portero = "portero";
-        String defensa = "defensa";
-        String medio = "medio";
-        String delantero = "delantero";
-        String base = "base";
-        String escolta = "escolta";
-        String alero = "alero";
-        String pivot = "pivot";
+        String phone_contact = "Móvil:";
+        String email_contact = "Email:";
+        String football = "Fútbol:";
+        String basketball = "Baloncesto:";
+        List<String> positions = new ArrayList<>();
 
         if (name.isEmpty() || surname.isEmpty() || phone.isEmpty() && email.isEmpty() || !cb_position1_football.isChecked() && !cb_position2_football.isChecked() && !cb_position3_football.isChecked() && !cb_position4_football.isChecked() && !cb_position1_basketball.isChecked() && !cb_position2_basketball.isChecked() && !cb_position3_basketball.isChecked() && !cb_position4_basketball.isChecked()) {
             Toast.makeText(this, "Rellene los campos", Toast.LENGTH_SHORT).show();
         } else {
             Intent pantallaSecundaria = new Intent(this, MainActivity2.class);
             if (!et_phone.getText().toString().isEmpty()) {
-                pantallaSecundaria.putExtra(MAINACTIVITY_PHONE, phone);
+                pantallaSecundaria.putExtra(MAINACTIVITY_SELECTED_CONTACT, phone_contact);
+                pantallaSecundaria.putExtra(MAINACTIVITY_CONTACT, phone);
             }
             if (!et_email.getText().toString().isEmpty()) {
-                pantallaSecundaria.putExtra(MAINACTIVITY_EMAIL, email);
+                pantallaSecundaria.putExtra(MAINACTIVITY_SELECTED_CONTACT, email_contact);
+                pantallaSecundaria.putExtra(MAINACTIVITY_CONTACT, email);
             }
             if (cb_position1_football.isChecked()) {
-                pantallaSecundaria.putExtra(MAINACTIVITY_PORTERO, portero);
+                positions.add(cb_position1_football.getText().toString());
+                pantallaSecundaria.putExtra(MAINACTIVITY_SPORT, football);
+                pantallaSecundaria.putExtra(MAINACTIVITY_POSITION, String.valueOf(positions));
             }
             if (cb_position2_football.isChecked()) {
-                pantallaSecundaria.putExtra(MAINACTIVITY_DEFENSA, defensa);
+                positions.add(cb_position2_football.getText().toString());
+                pantallaSecundaria.putExtra(MAINACTIVITY_SPORT, football);
+                pantallaSecundaria.putExtra(MAINACTIVITY_POSITION, String.valueOf(positions));
             }
             if (cb_position3_football.isChecked()) {
-                pantallaSecundaria.putExtra(MAINACTIVITY_MEDIO, medio);
+                positions.add(cb_position3_football.getText().toString());
+                pantallaSecundaria.putExtra(MAINACTIVITY_SPORT, football);
+                pantallaSecundaria.putExtra(MAINACTIVITY_POSITION, String.valueOf(positions));
             }
             if (cb_position4_football.isChecked()) {
-                pantallaSecundaria.putExtra(MAINACTIVITY_DELANTERO, delantero);
+                positions.add(cb_position4_football.getText().toString());
+                pantallaSecundaria.putExtra(MAINACTIVITY_SPORT, football);
+                pantallaSecundaria.putExtra(MAINACTIVITY_POSITION, String.valueOf(positions));
             }
             if (cb_position1_basketball.isChecked()) {
-                pantallaSecundaria.putExtra(MAINACTIVITY_BASE, base);
+                positions.add(cb_position1_basketball.getText().toString());
+                pantallaSecundaria.putExtra(MAINACTIVITY_SPORT, basketball);
+                pantallaSecundaria.putExtra(MAINACTIVITY_POSITION, String.valueOf(positions));
             }
             if (cb_position2_basketball.isChecked()) {
-                pantallaSecundaria.putExtra(MAINACTIVITY_ESCOLTA, escolta);
+                positions.add(cb_position2_basketball.getText().toString());
+                pantallaSecundaria.putExtra(MAINACTIVITY_SPORT, basketball);
+                pantallaSecundaria.putExtra(MAINACTIVITY_POSITION, String.valueOf(positions));
             }
             if (cb_position3_basketball.isChecked()) {
-                pantallaSecundaria.putExtra(MAINACTIVITY_ALERO, alero);
+                positions.add(cb_position3_basketball.getText().toString());
+                pantallaSecundaria.putExtra(MAINACTIVITY_SPORT, basketball);
+                pantallaSecundaria.putExtra(MAINACTIVITY_POSITION, String.valueOf(positions));
             }
             if (cb_position4_basketball.isChecked()) {
-                pantallaSecundaria.putExtra(MAINACTIVITY_PIVOT, pivot);
+                positions.add(cb_position4_basketball.getText().toString());
+                pantallaSecundaria.putExtra(MAINACTIVITY_SPORT, basketball);
+                pantallaSecundaria.putExtra(MAINACTIVITY_POSITION, String.valueOf(positions));
             }
             pantallaSecundaria.putExtra(MAINACTIVITY_NAME, name);
             pantallaSecundaria.putExtra(MAINACTIVITY_SURNAME, surname);
-            pantallaSecundaria.putExtra(MAINACTIVITY_FOOTBALL, football);
-            pantallaSecundaria.putExtra(MAINACTIVITY_BASKETBALL, basketball);
             startActivity(pantallaSecundaria);
         }
 
--- FormularioDeportivo/app/src/main/java/com/example/formulariodeportivo/MainActivity2.java
+++ FormularioDeportivo/app/src/main/java/com/example/formulariodeportivo/MainActivity2.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -19,47 +18,23 @@ public class MainActivity2 extends AppCompatActivity {
 
         String name = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_NAME);
         String surname = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_SURNAME);
-        String phone = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_PHONE);
-        String email = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_EMAIL);
-        String football = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_FOOTBALL);
-        String basketball = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_BASKETBALL);
-        String portero = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_PORTERO);
-        String defensa = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_DEFENSA);
-        String medio = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_MEDIO);
-        String delantero = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_DELANTERO);
-        String base = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_BASE);
-        String escolta = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_ESCOLTA);
-        String alero = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_ALERO);
-        String pivot = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_PIVOT);
+        String selected_contact = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_SELECTED_CONTACT);
+        String contact = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_CONTACT);
+        String sport = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_SPORT);
+        String position = pantallaPrincipal.getStringExtra(MainActivity.MAINACTIVITY_POSITION);
 
         TextView tv_name_result = (TextView) findViewById(R.id.tv_name_result);
         tv_name_result.setText(name);
         TextView tv_surname_result = (TextView) findViewById(R.id.tv_surname_result);
         tv_surname_result.setText(surname);
-        TextView tv_phone_result = (TextView) findViewById(R.id.tv_phone_result);
-        tv_phone_result.setText(phone);
-        TextView tv_email_result = (TextView) findViewById(R.id.tv_email_result);
-        tv_email_result.setText(email);
-        TextView tv_football_result = (TextView) findViewById(R.id.tv_res_football);
-        tv_football_result.setText(football);
-        TextView tv_basketball_result = (TextView) findViewById(R.id.tv_res_basketball);
-        tv_basketball_result.setText(basketball);
-        TextView tv_portero_result = (TextView) findViewById(R.id.tv_portero);
-        tv_portero_result.setText(portero);
-        TextView tv_defensa_result = (TextView) findViewById(R.id.tv_defensa);
-        tv_defensa_result.setText(defensa);
-        TextView tv_medio_result = (TextView) findViewById(R.id.tv_medio);
-        tv_medio_result.setText(medio);
-        TextView tv_delantero_result = (TextView) findViewById(R.id.tv_delantero);
-        tv_delantero_result.setText(delantero);
-        TextView tv_base_result = (TextView) findViewById(R.id.tv_base);
-        tv_base_result.setText(base);
-        TextView tv_escolta_result = (TextView) findViewById(R.id.tv_escolta);
-        tv_escolta_result.setText(escolta);
-        TextView tv_alero_result = (TextView) findViewById(R.id.tv_alero);
-        tv_alero_result.setText(alero);
-        TextView tv_pivot_result = (TextView) findViewById(R.id.tv_pivot);
-        tv_pivot_result.setText(pivot);
+        TextView tv_selected_contact_result = (TextView) findViewById(R.id.tv_res_contact);
+        tv_selected_contact_result.setText(selected_contact);
+        TextView tv_contact_result = (TextView) findViewById(R.id.tv_contact_result);
+        tv_contact_result.setText(contact);
+        TextView tv_sport_result = (TextView) findViewById(R.id.tv_sport_result);
+        tv_sport_result.setText(sport);
+        TextView tv_position_result = (TextView) findViewById(R.id.tv_position_result);
+        tv_position_result.setText(position);
     }
 
     public void returnToMainActivity(View v) {
