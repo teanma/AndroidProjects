558152e8e7528cd727fc41900c85b7ff18dcceb1
teanma <mardaleteodorandrei@gmail.com>
2021-12-02 22:42:05 +0100

    Update
    Upload

--- ListViewExample/app/src/main/java/com/example/listviewexample/MainActivity.java
+++ ListViewExample/app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -16,16 +16,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected final static String MAINACTIVITY_WINDOWS7 = "com.example.listviewexample.windows7";
-    protected final static String MAINACTIVITY_WINDOWS10 = "com.example.listviewexample.windows10";
-    protected final static String MAINACTIVITY_WINDOWS11 = "com.example.listviewexample.windows11";
-    protected final static String MAINACTIVITY_UBUNTU = "com.example.listviewexample.ubuntu";
-    protected final static String MAINACTIVITY_RED_HAT = "com.example.listviewexample.red_hat";
-    protected final static String MAINACTIVITY_LINUX_OTHERS = "com.example.listviewexample.linux_others";
-    protected final static String MAINACTIVITY_IOS = "com.example.listviewexample.ios";
-    protected final static String MAINACTIVITY_OTHERS = "com.example.listviewexample.others";
+    protected final static String MAINACTIVITY_OPERATING_SYSTEM = "com.example.listviewexample.operating_system";
+    protected final static String MAINACTIVITY_VERSION = "com.example.listviewexample.version";
 
     TextView tv_select_fruit;
+    ListView lv_operating_systems;
     RadioButton rb_windows7, rb_windows10, rb_windows11, rb_ubuntu, rb_red_hat, rb_linux_others;
     RadioGroup rg_windows_versions, rg_linux_versions;
     Button btn_load_info;
@@ -35,46 +30,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initialize();
+
         rg_windows_versions.setVisibility(View.GONE);
         rg_linux_versions.setVisibility(View.GONE);
 
-        initialize();
+        ListView lv_operating_systems = (ListView) findViewById(R.id.lv_operating_systems);
 
-        ListView lv_fruits = (ListView) findViewById(R.id.lv_fruits);
+        String[] os = {"Windows", "Linux", "iOS", "Others"};
 
-        String[] fruits = {"Windows", "Linux", "iOS", "Others"};
+        ArrayAdapter aa_os = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, os);
 
-        ArrayAdapter aa_fruits = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fruits);
+        lv_operating_systems.setAdapter(aa_os);
 
-        lv_fruits.setAdapter(aa_fruits);
-
-        lv_fruits.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        lv_operating_systems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String selected = parent.getSelectedItem().toString();
+                Toast.makeText(parent.getContext(), "OS selected " + selected, Toast.LENGTH_SHORT).show();
                 if (selected.equals("Windows")) {
                     rg_windows_versions.setVisibility(View.VISIBLE);
-                    rg_linux_versions.setVisibility(View.GONE);
-                    Toast.makeText(parent.getContext(), "You have selected the option: " + selected, Toast.LENGTH_SHORT).show();
                 } else if (selected.equals("Linux")) {
                     rg_linux_versions.setVisibility(View.VISIBLE);
-                    rg_windows_versions.setVisibility(View.GONE);
-                    Toast.makeText(parent.getContext(), "You have selected the option: " + selected, Toast.LENGTH_SHORT).show();
-                } else if (selected.equals("iOS")) {
-                    rg_linux_versions.setVisibility(View.GONE);
-                    rg_windows_versions.setVisibility(View.GONE);
-                    Toast.makeText(parent.getContext(), "You have selected the option: " + selected, Toast.LENGTH_SHORT).show();
                 } else {
                     rg_linux_versions.setVisibility(View.GONE);
                     rg_windows_versions.setVisibility(View.GONE);
-                    Toast.makeText(parent.getContext(), "You have selected the option: " + selected, Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
     public void initialize() {
-        tv_select_fruit = (TextView) findViewById(R.id.tv_selecct_fruit);
+        tv_select_fruit = (TextView) findViewById(R.id.tv_select_os);
+        lv_operating_systems = (ListView) findViewById(R.id.lv_operating_systems);
         rb_windows7 = (RadioButton) findViewById(R.id.rb_windows7);
         rb_windows10 = (RadioButton) findViewById(R.id.rb_windows10);
         rb_windows11 = (RadioButton) findViewById(R.id.rb_windows11);
@@ -87,35 +75,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void loadInfo(View v) {
+        String windows = "Windows";
         String windows7 = "Windows 7";
         String windows10 = "Windows 10";
         String windows11 = "Windows 11";
+        String linux = "Linux";
         String ubuntu = "Ubuntu";
         String red_hat = "RedHat";
-        String linux_others = "Another linux's version";
+        String linux_others = "Another Linux version";
         String iOS = "iOS";
         String others = "Others";
 
         Intent secondaryScreen = new Intent(this, MainActivity2.class);
         if (rb_windows7.isChecked()) {
-            secondaryScreen.putExtra(MAINACTIVITY_WINDOWS7, windows7);
+            secondaryScreen.putExtra(MAINACTIVITY_OPERATING_SYSTEM, windows);
+            secondaryScreen.putExtra(MAINACTIVITY_VERSION, windows7);
         }
         if (rb_windows10.isChecked()) {
-            secondaryScreen.putExtra(MAINACTIVITY_WINDOWS10, windows10);
+            secondaryScreen.putExtra(MAINACTIVITY_OPERATING_SYSTEM, windows);
+            secondaryScreen.putExtra(MAINACTIVITY_VERSION, windows10);
         }
         if (rb_windows11.isChecked()) {
-            secondaryScreen.putExtra(MAINACTIVITY_WINDOWS11, windows11);
+            secondaryScreen.putExtra(MAINACTIVITY_OPERATING_SYSTEM, windows);
+            secondaryScreen.putExtra(MAINACTIVITY_VERSION, windows11);
         }
         if (rb_ubuntu.isChecked()) {
-            secondaryScreen.putExtra(MAINACTIVITY_UBUNTU, ubuntu);
+            secondaryScreen.putExtra(MAINACTIVITY_OPERATING_SYSTEM, linux);
+            secondaryScreen.putExtra(MAINACTIVITY_VERSION, ubuntu);
         }
         if (rb_red_hat.isChecked()) {
-            secondaryScreen.putExtra(MAINACTIVITY_RED_HAT, red_hat);
+            secondaryScreen.putExtra(MAINACTIVITY_OPERATING_SYSTEM, linux);
+            secondaryScreen.putExtra(MAINACTIVITY_VERSION, red_hat);
         }
         if (rb_linux_others.isChecked()) {
-            secondaryScreen.putExtra(MAINACTIVITY_LINUX_OTHERS, linux_others);
+            secondaryScreen.putExtra(MAINACTIVITY_OPERATING_SYSTEM, linux);
+            secondaryScreen.putExtra(MAINACTIVITY_VERSION, linux_others);
         }
-        secondaryScreen.putExtra(MAINACTIVITY_IOS, iOS);
-        secondaryScreen.putExtra(MAINACTIVITY_OTHERS, others);
+        secondaryScreen.putExtra(MAINACTIVITY_OPERATING_SYSTEM, iOS);
+        secondaryScreen.putExtra(MAINACTIVITY_OPERATING_SYSTEM, others);
+        startActivity(secondaryScreen);
     }
 }
--- ListViewExample/app/src/main/java/com/example/listviewexample/MainActivity2.java
+++ ListViewExample/app/src/main/java/com/example/listviewexample/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.listviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent mainScreen = getIntent();
+
+        String operating_system = mainScreen.getStringExtra(MainActivity.MAINACTIVITY_OPERATING_SYSTEM);
+        String version = mainScreen.getStringExtra(MainActivity.MAINACTIVITY_VERSION);
+
+        TextView tv_selected_os = (TextView) findViewById(R.id.tv_selected_os);
+        tv_selected_os.setText(operating_system);
+        TextView tv_selected_version = (TextView) findViewById(R.id.tv_selected_version);
+        tv_selected_version.setText(version);
+
     }
 }
