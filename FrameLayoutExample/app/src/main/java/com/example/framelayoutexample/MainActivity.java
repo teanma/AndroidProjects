3e68339dad626b207b7413c560048a8381ceb07d
teanma <mardaleteodorandrei@gmail.com>
2021-12-15 23:35:42 +0100

    Update
    Upload

--- FrameLayoutExample/app/src/main/java/com/example/framelayoutexample/MainActivity.java
+++ FrameLayoutExample/app/src/main/java/com/example/framelayoutexample/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView iv_dell, iv_hp;
     EditText et_top_right, et_left_center, et_right_bottom;
-    Button btn_move_widgets;
+    CheckBox cb_move_widgets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         et_left_center = (EditText) findViewById(R.id.et_left_center);
         et_right_bottom = (EditText) findViewById(R.id.et_right_bottom);
 
-        btn_move_widgets = (Button) findViewById(R.id.btn_move_widgets);
+        cb_move_widgets = (CheckBox) findViewById(R.id.cb_move_widgets);
     }
 
     public void moveWidgets(View v) {
@@ -45,16 +46,38 @@ public class MainActivity extends AppCompatActivity {
         FrameLayout.LayoutParams left_center_params = (FrameLayout.LayoutParams) et_left_center.getLayoutParams();
         FrameLayout.LayoutParams right_bottom_params = (FrameLayout.LayoutParams) et_right_bottom.getLayoutParams();
 
-        dell_params.gravity = Gravity.BOTTOM | Gravity.RIGHT;
-        iv_dell.setLayoutParams(dell_params);
-        hp_params.gravity = Gravity.RIGHT | Gravity.TOP;
-        iv_hp.setLayoutParams(hp_params);
+        if (cb_move_widgets.isChecked()) {
+            dell_params.gravity = Gravity.BOTTOM | Gravity.RIGHT;
+            iv_dell.setLayoutParams(dell_params);
+            hp_params.gravity = Gravity.RIGHT | Gravity.TOP;
+            iv_hp.setLayoutParams(hp_params);
 
-        top_right_params.gravity = Gravity.LEFT | Gravity.TOP;
-        et_top_right.setLayoutParams(top_right_params);
-        left_center_params.gravity = Gravity.CENTER | Gravity.RIGHT;
-        et_left_center.setLayoutParams(left_center_params);
-        right_bottom_params.gravity = Gravity.BOTTOM | Gravity.LEFT;
-        et_right_bottom.setLayoutParams(right_bottom_params);
+            et_top_right.setText("Top left");
+            et_left_center.setText("Right center");
+            et_right_bottom.setText("Left bottom");
+
+            top_right_params.gravity = Gravity.LEFT | Gravity.TOP;
+            et_top_right.setLayoutParams(top_right_params);
+            left_center_params.gravity = Gravity.CENTER | Gravity.RIGHT;
+            et_left_center.setLayoutParams(left_center_params);
+            right_bottom_params.gravity = Gravity.BOTTOM | Gravity.LEFT;
+            et_right_bottom.setLayoutParams(right_bottom_params);
+        } else {
+            dell_params.gravity = Gravity.LEFT | Gravity.TOP;
+            iv_dell.setLayoutParams(dell_params);
+            hp_params.gravity = Gravity.BOTTOM | Gravity.LEFT;
+            iv_hp.setLayoutParams(hp_params);
+
+            et_top_right.setText("Top right");
+            et_left_center.setText("Left center");
+            et_right_bottom.setText("Right bottom");
+
+            top_right_params.gravity = Gravity.RIGHT | Gravity.TOP;
+            et_top_right.setLayoutParams(top_right_params);
+            left_center_params.gravity = Gravity.CENTER | Gravity.LEFT;
+            et_left_center.setLayoutParams(left_center_params);
+            right_bottom_params.gravity = Gravity.BOTTOM | Gravity.RIGHT;
+            et_right_bottom.setLayoutParams(right_bottom_params);
+        }
     }
 }
