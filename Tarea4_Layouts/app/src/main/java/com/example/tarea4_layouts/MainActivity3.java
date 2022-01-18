f1bf370f6c089c70c3ace6045355d4b44d0427e7
teanma <mardaleteodorandrei@gmail.com>
2022-01-19 00:33:29 +0100

    Update
    Upload

--- Tarea4_Layouts/app/src/main/java/com/example/tarea4_layouts/MainActivity3.java
+++ Tarea4_Layouts/app/src/main/java/com/example/tarea4_layouts/MainActivity3.java
@@ -22,42 +22,27 @@ public class MainActivity3 extends AppCompatActivity {
         setContentView(R.layout.activity_main3);
         init();
 
-        tv_respuesta_jugador.setVisibility(View.GONE);
-        tv_respuesta_correcta.setVisibility(View.GONE);
-        btn_respuesta_incorrecta.setVisibility(View.GONE);
-
         Intent pantallaSecundaria = getIntent();
 
         String nick = pantallaSecundaria.getStringExtra(MainActivity.MAINACTIVITY_NICK);
         String respuesta = pantallaSecundaria.getStringExtra(MainActivity2.MAINACTIVITY2_RESPUESTA);
 
-        if(respuesta.equals("Opensource Framework, usado para escribir y ejecutar tests")) {
-            Button btn_respuesta_correcta = (Button) findViewById(R.id.btn_respuesta_correcta);
+        if (respuesta.equals("Opensource Framework, usado para escribir y ejecutar tests")) {
+            btn_respuesta_correcta = (Button) findViewById(R.id.btn_respuesta_correcta);
             btn_respuesta_correcta.setText(respuesta);
             tv_mensaje_respuesta.setText("Enhorabuena, " + nick + ", has acertado");
-            btn_respuesta_correcta.setVisibility(View.VISIBLE);
-            tv_respuesta_correcta.setVisibility(View.VISIBLE);
-        } else if(respuesta.equals("Java Development Kit")) {
-            Button btn_respuesta_incorrecta = (Button) findViewById(R.id.btn_respuesta_incorrecta);
-            btn_respuesta_incorrecta.setText(respuesta);
-            tv_mensaje_respuesta.setText("Lo siento, " + nick + ", respuesta incorrecta");
-            tv_respuesta_jugador.setVisibility(View.VISIBLE);
-            tv_respuesta_correcta.setVisibility(View.VISIBLE);
-            btn_respuesta_incorrecta.setVisibility(View.VISIBLE);
-        } else if (respuesta.equals("Opensource Framework, el cual traduce bytecode a código binario")) {
-            Button btn_respuesta_incorrecta = (Button) findViewById(R.id.btn_respuesta_incorrecta);
-            btn_respuesta_incorrecta.setText(respuesta);
-            tv_mensaje_respuesta.setText("Lo siento, " + nick + ", respuesta incorrecta");
-            tv_respuesta_jugador.setVisibility(View.VISIBLE);
-            tv_respuesta_correcta.setVisibility(View.VISIBLE);
-            btn_respuesta_incorrecta.setVisibility(View.VISIBLE);
-        } else if (respuesta.equals("Ninguno de los anteriores")) {
-            Button btn_respuesta_incorrecta = (Button) findViewById(R.id.btn_respuesta_incorrecta);
-            btn_respuesta_incorrecta.setText(respuesta);
-            tv_mensaje_respuesta.setText("Lo siento, " + nick + ", respuesta incorrecta");
-            tv_respuesta_jugador.setVisibility(View.VISIBLE);
-            tv_respuesta_correcta.setVisibility(View.VISIBLE);
-            btn_respuesta_incorrecta.setVisibility(View.VISIBLE);
+            //btn_respuesta_correcta.setVisibility(View.VISIBLE);
+            //tv_respuesta_jugador.setVisibility(View.VISIBLE);
+            btn_respuesta_incorrecta.setVisibility(View.GONE);
+            tv_respuesta_correcta.setVisibility(View.GONE);
+        } else {
+            //Button btn_respuesta_incorrecta = (Button) findViewById(R.id.btn_respuesta_incorrecta);
+            //btn_respuesta_incorrecta.setText(respuesta);
+            tv_mensaje_respuesta.setText("Lo sentimos, " + nick + ", has fallado");
+            //btn_respuesta_correcta.setVisibility(View.VISIBLE);
+            //tv_respuesta_jugador.setVisibility(View.VISIBLE);
+            //btn_respuesta_incorrecta.setVisibility(View.VISIBLE);
+            //tv_respuesta_correcta.setVisibility(View.VISIBLE);
         }
     }
 
