228d30ab022d50e2eabcfe64d1aa028dcea227b0
teanma <mardaleteodorandrei@gmail.com>
2022-02-18 12:16:16 +0100

    Update
    Upload

--- DatabaseExample/app/src/main/java/com/example/databaseexample/MainActivity.java
+++ DatabaseExample/app/src/main/java/com/example/databaseexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.databaseexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -36,6 +37,16 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this, "Tabla comentarios creada.", Toast.LENGTH_SHORT).show();
 
+        // Opción 1: Insert mediante execSQL
+        String insertComments = "INSERT INTO comments (user, comment) VALUES ('admin', 'Administrador del sistema')";
+        database.execSQL(insertComments);
+
+        // Opción 2: Insert mediante contentValues
+        ContentValues insertCommentsV2 = new ContentValues();
+        insertCommentsV2.put("user", "usuario");
+        insertCommentsV2.put("comment", "Usuario normal del sistema");
+        database.insert("comments", null, insertCommentsV2);
+
         //database.close();
     }
 
@@ -52,5 +63,23 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Introduce un nombre para la base de datos.", Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    public void executeSentence(View vista) {
+
+        EditText et_sentence = (EditText) findViewById(R.id.et_sentence);
+
+        DBHelper dbHelperCustom = new DBHelper(this, "lawyers.sqlite", null, 1);
+
+        SQLiteDatabase database = dbHelperCustom.getWritableDatabase();
+
+        String sentence = et_sentence.getText().toString();
+
+        if(!et_sentence.getText().toString().equals("")) {
+            database.execSQL(sentence);
+            Toast.makeText(this, "Sentencia añadida a la base de datos", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Introduce una sentencia", Toast.LENGTH_SHORT).show();
+        }
     }
 }
